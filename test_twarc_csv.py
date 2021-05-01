97c0664aa3729a97880e7b552e007454a44450bf
Igor Brigadir <igor.brigadir@gmail.com>
2021-05-02 00:22:12 +0100

    refactor test

--- test_twarc_csv.py
+++ test_twarc_csv.py
@@ -7,9 +7,9 @@ from click.testing import CliRunner
 runner = CliRunner()
 test_data = pathlib.Path('test-data')
 
-def test_noflat():
-    input_file = test_data / "noflat.jsonl"
-    output_file = test_data / "noflat.csv"
+def _process_file(fname):
+    input_file = test_data / f"{fname}.jsonl"
+    output_file = test_data / f"{fname}.csv"
     if output_file.is_file():
         output_file.unlink()
     result = runner.invoke(twarc_csv.csv, [str(input_file), str(output_file)])
@@ -18,13 +18,11 @@ def test_noflat():
     assert len(df) > 0
     assert type(df['text']) == pandas.Series
 
+def test_noflat():
+    _process_file("noflat")
+
 def test_flat():
-    input_file = test_data / "flat.jsonl"
-    output_file = test_data / "flat.csv"
-    if output_file.is_file():
-        output_file.unlink()
-    runner.invoke(twarc_csv.csv, [str(input_file), str(output_file)])
-    assert output_file.is_file()
-    df = pandas.read_csv(output_file)
-    assert len(df) > 0
-    assert type(df['text']) == pandas.Series
+    _process_file("flat")
+
+def test_withheld():
+    _process_file("withheld")
