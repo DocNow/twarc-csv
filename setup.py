bc418d915d580ab0ec060f6c2ba69547e72b10f4
Igor Brigadir <igor.brigadir@gmail.com>
2021-05-19 18:06:31 +0100

    v0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="twarc-csv",
-    version="0.3.0",
+    version="0.3.1",
     url="https://github.com/docnow/twarc-csv",
     author="Igor Brigadir",
     author_email="igor.brigadir@gmail.com",
