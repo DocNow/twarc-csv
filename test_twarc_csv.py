ef01a4e8f2c86c8f6fe87e3f89dd141b69473218
Igor Brigadir <igor.brigadir@gmail.com>
2021-10-22 15:43:24 +0100

    add more tests

--- test_twarc_csv.py
+++ test_twarc_csv.py
@@ -8,12 +8,18 @@ runner = CliRunner()
 test_data = pathlib.Path("test-data")
 
 
-def _process_file(fname, expected=None):
+def _process_file(fname, expected=None, extra=""):
     input_file = test_data / f"{fname}.jsonl"
     output_file = test_data / f"{fname}.csv"
     if output_file.is_file():
         output_file.unlink()
-    result = runner.invoke(twarc_csv.csv, [str(input_file), str(output_file)])
+
+    result = runner.invoke(
+        twarc_csv.csv, f"{str(input_file)} {str(output_file)}{extra}"
+    )
+
+    print(result.output)
+
     assert output_file.is_file()
     df = pandas.read_csv(output_file)
     if expected:
@@ -21,6 +27,8 @@ def _process_file(fname, expected=None):
     else:
         assert len(df) > 0
     assert type(df["text"]) == pandas.Series
+    if output_file.is_file():
+        output_file.unlink()
 
 
 def test_empty():
@@ -39,7 +47,23 @@ def test_flat():
 
 
 def test_expected_tweets():
-    _process_file("two_tweets", 2)
+    _process_file("two_tweets", 2, extra=" --inline-referenced-tweets")
+
+
+def test_2sets():
+    _process_file("2sets")
+
+
+def test_brexit():
+    _process_file("2sets")
+
+
+def test_kpop():
+    _process_file("kpop")
+
+
+def test_streaming_output_with_error():
+    _process_file("streaming_output_with_error")
 
 
 def test_withheld():
