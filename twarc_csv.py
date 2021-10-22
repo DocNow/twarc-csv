7a9112925cce06f96696b77dcc6b3e47a11c5f50
Igor Brigadir <igor.brigadir@gmail.com>
2021-10-22 15:56:02 +0100

    rework CSV output

--- twarc_csv.py
+++ twarc_csv.py
@@ -6,7 +6,7 @@ import logging
 import itertools
 import pandas as pd
 from tqdm.auto import tqdm
-from collections import OrderedDict, ChainMap
+from collections import ChainMap
 from more_itertools import ichunked
 from twarc import ensure_flattened
 
@@ -89,7 +89,7 @@ __twarc.version
     "\n"
 )
 
-DEFAULT_USERS_COLUMNS = """id
+DEFAULT_USER_COLUMNS = """id
 created_at
 username
 name
@@ -120,103 +120,112 @@ __twarc.version
     "\n"
 )
 
+DEFAULT_COMPLIANCE_COLUMNS = """id
+action
+created_at
+redacted_at
+reason
+""".split(
+    "\n"
+)
 
-class CSVConverter:
+DEFAULT_COUNTS_COLUMNS = """start
+end
+count
+"""
+
+
+class DataFrameConverter:
+    """
+    Convert a set of JSON Objects into a Pandas DataFrame object.
+    You can call this directly on a small set of tweets, but memory is quickly consumed for larger datasets.
+
+    This class can accept individual tweets or whole response objects.
+
+    Args:
+        objects (iterable): JSON Objects to convert.
+        input_data_type (str): data type: `tweets` or `users` or `compliance` or `counts`
+    Returns:
+        DataFrame: The objects provided as a Pandas DataFrame.
+    """
+
     def __init__(
         self,
-        infile,
-        outfile,
+        input_data_type="tweets",
         json_encode_all=False,
-        json_encode_lists=True,
         json_encode_text=False,
+        json_encode_lists=True,
         inline_referenced_tweets=False,
+        merge_retweets=True,
         allow_duplicates=False,
-        input_tweet_columns=True,
-        input_users_columns=False,
         extra_input_columns="",
         output_columns="",
-        batch_size=100,
-        hide_progress=False,
+        dataset_ids=None,
+        counts=None,
     ):
-        self.infile = infile
-        self.outfile = outfile
         self.json_encode_all = json_encode_all
-        self.json_encode_lists = json_encode_lists
         self.json_encode_text = json_encode_text
+        self.json_encode_lists = json_encode_lists
         self.inline_referenced_tweets = inline_referenced_tweets
+        self.merge_retweets = merge_retweets
         self.allow_duplicates = allow_duplicates
-        self.batch_size = batch_size
-        self.dataset_ids = set()
-        self.hide_progress = (
-            infile.name == "<stdin>" or outfile.name == "<stdout>" or hide_progress
-        )
-        self.progress = tqdm(
-            unit="B",
-            unit_scale=True,
-            unit_divisor=1024,
-            total=os.stat(infile.name).st_size if not self.hide_progress else 1,
-            disable=self.hide_progress,
-        )
+
         self.columns = list()
-        if input_tweet_columns:
+        if "tweets" in input_data_type:
             self.columns.extend(
                 x for x in DEFAULT_TWEET_COLUMNS if x not in self.columns
             )
-        if input_users_columns:
+        if "users" in input_data_type:
             self.columns.extend(
-                x for x in DEFAULT_USERS_COLUMNS if x not in self.columns
+                x for x in DEFAULT_USER_COLUMNS if x not in self.columns
+            )
+        if "compliance" in input_data_type:
+            self.columns.extend(
+                x for x in DEFAULT_COMPLIANCE_COLUMNS if x not in self.columns
+            )
+        if "counts" in input_data_type:
+            self.columns.extend(
+                x for x in DEFAULT_COUNTS_COLUMNS if x not in self.columns
             )
         if extra_input_columns:
             self.columns.extend(
                 x for x in extra_input_columns.split(",") if x not in self.columns
             )
-
         self.output_columns = (
             output_columns.split(",") if output_columns else self.columns
         )
+        self.dataset_ids = dataset_ids if dataset_ids else set()
+        self.counts = (
+            counts
+            if counts
+            else {
+                "lines": 0,
+                "tweets": 0,
+                "referenced_tweets": 0,
+                "retweets": 0,
+                "quotes": 0,
+                "replies": 0,
+                "unavailable": 0,
+                "non_tweets": 0,
+                "parse_errors": 0,
+                "duplicates": 0,
+                "rows": 0,
+                "input_columns": len(self.columns),
+                "output_columns": len(output_columns),
+            }
+        )
 
-        self.counts = {
-            "lines": 0,
-            "tweets": 0,
-            "referenced_tweets": 0,
-            "unavailable": 0,
-            "non_tweets": 0,
-            "parse_errors": 0,
-            "duplicates": 0,
-            "rows": 0,
-            "input_columns": len(self.columns),
-            "output_columns": len(self.output_columns),
-        }
-
-    def _read_lines(self):
-        """
-        Generator for reading files line by line from a file. Progress bar is based on file size.
-        """
-        line = self.infile.readline()
-        while line:
-            self.counts["lines"] += 1
-            if line.strip() != "":
-                try:
-                    o = json.loads(line)
-                    yield o
-                except Exception as ex:
-                    self.counts["parse_errors"] += 1
-                    log.error(f"Error when trying to parse json: '{line}' {ex}")
-            if not self.hide_progress:
-                self.progress.update(self.infile.tell() - self.progress.n)
-            line = self.infile.readline()
-
-    def _generate_tweets(self, batch):
+    def _generate_tweets(self, objects):
         """
         Generate flattened tweets from a batch of parsed lines.
         """
-        for item in batch:
+        for item in objects:
             for tweet in ensure_flattened(item):
                 yield tweet
 
     def _inline_referenced_tweets(self, tweet):
         """
-        # (Optional) Insert referenced tweets into the main CSV as new rows
+        (Optional) Insert referenced tweets into the main CSV as new rows
         """
         if "referenced_tweets" in tweet and self.inline_referenced_tweets:
             for referenced_tweet in tweet["referenced_tweets"]:
@@ -234,23 +243,61 @@ class CSVConverter:
         yield self._format_tweet(tweet)
 
     def _format_tweet(self, tweet):
+        """
+        Make the tweet objects easier to deal with, removing extra info and changing the structure
+        """
+        # Make a copy of the original flattened tweet
+        tweet = copy.deepcopy(tweet)
         # Deal with pinned tweets for user datasets, `tweet` here is actually a user:
         # remove the tweet from a user dataset, pinned_tweet_id remains:
-        tweet = copy.deepcopy(tweet)
         tweet.pop("pinned_tweet", None)
         # Remove in_reply_to_user, in_reply_to_user_id remains:
         tweet.pop("in_reply_to_user", None)
-        # Deal with referenced_tweets
+
         if "referenced_tweets" in tweet:
+            # Extract Retweet only
+            rts = [t for t in tweet["referenced_tweets"] if t["type"] == "retweeted"]
+            retweeted_tweet = rts[-1] if rts else None
+            if retweeted_tweet and "author_id" in retweeted_tweet:
+                tweet["retweeted_user_id"] = retweeted_tweet["author_id"]
+
+            # Extract Quoted tweet
+            qts = [t for t in tweet["referenced_tweets"] if t["type"] == "quoted"]
+            quoted_tweet = qts[-1] if qts else None
+            if quoted_tweet and "author_id" in quoted_tweet:
+                tweet["quoted_user_id"] = quoted_tweet["author_id"]
+
+            # Process Retweets:
+            # If it's a native retweet, replace the "RT @user Text" with the original text, metrics, and entities, but keep the Author.
+            if retweeted_tweet and self.merge_retweets:
+                # A retweet inherits everything from retweeted tweet.
+                tweet["text"] = retweeted_tweet.pop("text", None)
+                tweet["entities"] = retweeted_tweet.pop("entities", None)
+                tweet["attachments"] = retweeted_tweet.pop("attachments", None)
+                tweet["context_annotations"] = retweeted_tweet.pop(
+                    "context_annotations", None
+                )
+                tweet["public_metrics"] = retweeted_tweet.pop("public_metrics", None)
+
             # reconstruct referenced_tweets object
             referenced_tweets = [
                 {r["type"]: {"id": r["id"]}} for r in tweet["referenced_tweets"]
             ]
             # leave behind references, but not the full tweets
+            # ChainMap flattens list into properties
             tweet["referenced_tweets"] = dict(ChainMap(*referenced_tweets))
         else:
             tweet["referenced_tweets"] = {}
+        # Remove `type` left over from referenced tweets
         tweet.pop("type", None)
+        # Remove empty objects
+        if "attachments" in tweet and not tweet["attachments"]:
+            tweet.pop("attachments", None)
+        if "entities" in tweet and not tweet["entities"]:
+            tweet.pop("entities", None)
+        if "public_metrics" in tweet and not tweet["public_metrics"]:
+            tweet.pop("public_metrics", None)
+
         return tweet
 
     def _process_tweets(self, tweets):
@@ -274,6 +321,10 @@ class CSVConverter:
                 self.counts["non_tweets"] += 1
 
     def _process_dataframe(self, _df):
+        """
+        Apply additional preprocessing to the DataFrame contents.
+        """
+
         # (Optional) json encode all
         if self.json_encode_all:
             _df = _df.applymap(json.dumps, na_action="ignore")
@@ -300,15 +351,15 @@ class CSVConverter:
                 )
         return _df
 
-    def _process_batch(self, batch):
+    def process(self, objects):
         """
-        Process a batch of lines into a tweet dataframe that will be merged with others.
+        Process the objects into a pandas dataframe.
         """
+
         tweet_batch = itertools.chain.from_iterable(
             self._process_tweets(self._inline_referenced_tweets(tweet))
-            for tweet in self._generate_tweets(batch)
+            for tweet in self._generate_tweets(objects)
         )
-
         _df = pd.json_normalize(list(tweet_batch), errors="ignore")
 
         # Check for mismatched columns
@@ -329,13 +380,62 @@ class CSVConverter:
 
         return self._process_dataframe(_df.reindex(columns=self.columns))
 
+
+class CSVConverter:
+    """
+    JSON Reader and CSV Writer. Converts a given file into CSV, splitting it into chunks, showing progress.
+    """
+
+    def __init__(
+        self,
+        infile,
+        outfile,
+        converter=DataFrameConverter(),
+        output_format="csv",
+        batch_size=100,
+        hide_progress=False,
+    ):
+        self.infile = infile
+        self.outfile = outfile
+        self.converter = converter
+        self.output_format = output_format
+        self.batch_size = batch_size
+        self.hide_progress = hide_progress
+        self.hide_progress = (
+            infile.name == "<stdin>" or outfile.name == "<stdout>" or hide_progress
+        )
+        self.progress = tqdm(
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+            total=os.stat(infile.name).st_size if not self.hide_progress else 1,
+            disable=self.hide_progress,
+        )
+
+    def _read_lines(self):
+        """
+        Generator for reading files line by line from a file. Progress bar is based on file size.
+        """
+        line = self.infile.readline()
+        while line:
+            self.converter.counts["lines"] += 1
+            if line.strip() != "":
+                try:
+                    o = json.loads(line)
+                    yield o
+                except Exception as ex:
+                    self.converter.counts["parse_errors"] += 1
+                    log.error(f"Error when trying to parse json: '{line}' {ex}")
+            if not self.hide_progress:
+                self.progress.update(self.infile.tell() - self.progress.n)
+            line = self.infile.readline()
+
     def _write_output(self, _df, first_batch):
         """
         Write out the dataframe chunk by chunk
 
         todo: take parameters from commandline for optional output formats.
         """
-
         if first_batch:
             mode = "w"
             header = True
@@ -343,11 +443,11 @@ class CSVConverter:
             mode = "a+"
             header = False
 
-        self.counts["rows"] += len(_df)
+        self.converter.counts["rows"] += len(_df)
         _df.to_csv(
             self.outfile,
             mode=mode,
-            columns=self.output_columns,
+            columns=self.converter.output_columns,
             index=False,
             header=header,
         )  # todo: (Optional) arguments for to_csv
@@ -360,7 +460,7 @@ class CSVConverter:
         # Flag for writing header & appending to CSV file
         first_batch = True
         for batch in ichunked(self._read_lines(), self.batch_size):
-            self._write_output(self._process_batch(batch), first_batch)
+            self._write_output(self.converter.process(batch), first_batch)
             first_batch = False
 
         self.progress.close()
@@ -370,9 +470,28 @@ class CSVConverter:
 @click.argument("infile", type=click.File("r", encoding="utf8"), default="-")
 @click.argument("outfile", type=click.File("w", encoding="utf8"), default="-")
 @click.option(
+    "--input-data-type",
+    required=False,
+    default="tweets",
+    help='Input data type - you can turn "tweets", "users", "counts" or "compliance" data into CSV.',
+    type=click.Choice(
+        ["tweets", "users", "counts", "compliance"], case_sensitive=False
+    ),
+)
+@click.option(
     "--json-encode-all/--no-json-encode-all",
     default=False,
     help="JSON encode / escape all fields. Default: no",
+)
+@click.option(
+    "--json-encode-text/--no-json-encode-text",
+    default=False,
+    help="Apply JSON encode / escape to text fields. Default: no",
+)
+@click.option(
+    "--inline-referenced-tweets/--no-inline-referenced-tweets",
+    default=False,
+    help="Output referenced tweets inline as separate rows. Default: no.",
 )
 @click.option(
     "--json-encode-lists/--no-json-encode-lists",
@@ -380,34 +499,20 @@ class CSVConverter:
     help="JSON encode / escape lists. Default: yes",
 )
 @click.option(
-    "--json-encode-text/--no-json-encode-text",
-    default=False,
-    help="JSON encode / escape text fields. Default: no",
-)
-@click.option(
-    "--inline-referenced-tweets/--no-inline-referenced-tweets",
-    default=False,
-    help="Output referenced tweets inline as separate rows. Default: no",
-)
-@click.option(
-    "--allow-duplicates/--no-allow-duplicates",
-    default=False,
-    help="Remove duplicate tweets by ID. Default: yes",
-)
-@click.option(
-    "--input-tweet-columns/--no-input-tweet-columns",
+    "--merge-retweets/--no-merge-retweets",
     default=True,
-    help="Use a default list of tweet column names in the input.  Default: yes",
+    help="Merge original tweet metadata into retweets. The Retweet Text, metrics and entities are merged from the original tweet. Default: Yes.",
 )
 @click.option(
-    "--input-users-columns/--no-input-users-columns",
+    "--allow-duplicates",
+    is_flag=True,
     default=False,
-    help="Use a default list of user column names in the input. Only modify this if you have a dataset of users as opposed to tweets. Default: no",
+    help="List every tweets as is, including duplicates. Default: No, only unique tweets per row. Retweets are not duplicates.",
 )
 @click.option(
     "--extra-input-columns",
     default="",
-    help="Manually specify extra input columns. Comma separated string. Only modify this if you have processed the json yourself. Default is blank, no extra input columns.",
+    help="Manually specify extra input columns. Comma separated string. Only modify this if you have processed the json yourself. Default output is all available object columns, no extra input columns.",
 )
 @click.option(
     "--output-columns",
@@ -421,30 +526,31 @@ class CSVConverter:
     help="How many lines to process per chunk. Default is 100. Reduce this if output is slow.",
 )
 @click.option(
-    "--show-stats/--no-show-stats",
-    default=True,
-    help="Show stats about the dataset on completion. Default is show. Always hidden if you're using stdin / stdout pipes.",
+    "--hide-stats",
+    is_flag=True,
+    default=False,
+    help="Hide stats about the dataset on completion. Always hidden if you're using stdin / stdout pipes.",
 )
 @click.option(
     "--hide-progress",
     is_flag=True,
     default=False,
-    help="Hide the Progress bar. Default: show progress.",
+    help="Hide the Progress bar. Always hidden if you're using stdin / stdout pipes.",
 )
 def csv(
     infile,
     outfile,
+    input_data_type,
     json_encode_all,
-    json_encode_lists,
     json_encode_text,
+    json_encode_lists,
     inline_referenced_tweets,
+    merge_retweets,
     allow_duplicates,
-    input_tweet_columns,
-    input_users_columns,
     extra_input_columns,
     output_columns,
     batch_size,
-    show_stats,
+    hide_stats,
     hide_progress,
 ):
     """
@@ -474,24 +580,29 @@ def csv(
         )
         return
 
-    converter = CSVConverter(
-        infile,
-        outfile,
-        json_encode_all,
-        json_encode_lists,
-        json_encode_text,
-        inline_referenced_tweets,
-        allow_duplicates,
-        input_tweet_columns,
-        input_users_columns,
-        extra_input_columns,
-        output_columns,
-        batch_size,
-        hide_progress,
+    converter = DataFrameConverter(
+        input_data_type=input_data_type,
+        json_encode_all=json_encode_all,
+        json_encode_text=json_encode_text,
+        json_encode_lists=json_encode_lists,
+        inline_referenced_tweets=inline_referenced_tweets,
+        merge_retweets=merge_retweets,
+        allow_duplicates=allow_duplicates,
+        extra_input_columns=extra_input_columns,
+        output_columns=output_columns,
     )
-    converter.process()
 
-    if show_stats and outfile.name != "<stdout>":
+    writer = CSVConverter(
+        infile=infile,
+        outfile=outfile,
+        converter=converter,
+        output_format="csv",
+        batch_size=batch_size,
+        hide_progress=hide_progress,
+    )
+    writer.process()
+
+    if not hide_stats and outfile.name != "<stdout>":
 
         errors = (
             click.style(
